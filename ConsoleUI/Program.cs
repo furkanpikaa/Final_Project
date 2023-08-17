fb8b3ed8c12df1b705f0dd6510bb8a5b7c3b5d67
Pikaa <furkansrds@gmail.com>
2023-08-17 23:00:58 +0300

    Core Katmanı

--- ConsoleUI/Program.cs
+++ ConsoleUI/Program.cs
@@ -11,13 +11,30 @@ namespace ConsoleUI
         //Open Closed Principle
         static void Main(string[] args)
         {
+            //ProductTest();
+
+            CategoryTest();
+
+            Console.ReadLine();
+        }
+
+        private static void CategoryTest()
+        {
+            CategoryManager categoryManager = new CategoryManager(new EfCategoryDal());
+            foreach (var category in categoryManager.GetAll())
+            {
+                Console.WriteLine(category.CategoryName);
+            }
+        }
+
+        private static void ProductTest()
+        {
             ProductManager productManager = new ProductManager(new EfProductDal());
 
-            foreach (var product in productManager.GetByUnitPrice(40,100))
+            foreach (var product in productManager.GetAllByCategoryId(3))
             {
                 Console.WriteLine(product.ProductName);
             }
-            Console.ReadLine();
         }
     }
 }
--- Core/DataAccess/IEntityRepository.cs
+++ Core/DataAccess/IEntityRepository.cs
@@ -1,11 +1,13 @@
-﻿using Entities.Abstract;
+﻿using Core.Entities;
 using System;
 using System.Collections.Generic;
 using System.Linq.Expressions;
 using System.Text;
 
-namespace DataAccess.Abstract
+namespace Core.DataAccess
 {
+    //Core katmanı diğer katmanlara bağımlı olmaz.
+
     public interface IEntityRepository<T> where T:class,IEntity,new()
     {
         List<T> GetAll(Expression<Func<T, bool>> filter = null);
